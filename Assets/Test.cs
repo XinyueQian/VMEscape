84d01aa2a023337f1ab529f918b4f55e0c9c2a13
XinyueQian <xinyue_qian@brown.edu>
2020-10-07 03:43:27 +0800

    First commit, test

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private string str="Hello Virtual Museum";
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
